43daf6ee4609d08c57efefad24a43e20817f2640
Cory Hayes <corymhayes@gmail.com>
2026-03-31 21:34:03 -0500

    - debugging cloudflare

--- src/worker/db/queries/delete.ts
+++ src/worker/db/queries/delete.ts
@@ -1,11 +1,11 @@
-import { eq } from "drizzle-orm";
-import { createDB } from "..";
-import { type SelectApplication, applicationTable } from "../schema";
+// import { eq } from "drizzle-orm";
+// import { createDB } from "..";
+// import { type SelectApplication, applicationTable } from "../schema";
 
-export async function deleteApplication(
-  env: string,
-  id: SelectApplication["id"],
-) {
-  const db = createDB(env);
-  await db.delete(applicationTable).where(eq(applicationTable.id, id));
-}
+// export async function deleteApplication(
+//   env: string,
+//   id: SelectApplication["id"],
+// ) {
+//   const db = createDB(env);
+//   await db.delete(applicationTable).where(eq(applicationTable.id, id));
+// }
